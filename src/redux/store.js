7570ec248e418d0787be606addf6a4c7f2e2416d
song-chaeyoung <153435389+song-chaeyoung@users.noreply.github.com>
2024-12-05 14:18:24 +0900

    Edit Nefliy Crush

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,6 @@ import { createStore, applyMiddleware } from "redux";
 import { thunk } from "redux-thunk";
 import rootReducer from "./reducers";
 
-const store = createStore(
-  rootReducer
-  // composeWithDevTools(applyMiddleware(thunk))
-);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
